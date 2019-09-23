5dbd2d0fc62648360fedbf27b96dbcf17f940cb1
bsahu44 <45997873+bsahu44@users.noreply.github.com>
2019-09-23 20:22:15 +0530

    Update MashUptest.js

--- MashUptest.js
+++ MashUptest.js
@@ -1,46 +1,47 @@
-//Parameters to pass
-//analysisPath : '/ACE India/Public/Standardisation_Demo_BI_Portal'
-//parameters : 'flattenDXP="Y";VizTabName="Page1";vizName="Bar Chart (Dashboard 1)";'
-//divID : 'container'
-//vizDetails : 'Page1=Bar Chart (Dashboard 1)'
+var c_serverUrl = "http://internal.bi.verisk.com/spotfire/wp/";
 
-function openDoc(analysisPath, divID, vizDetails) {
-	var c_serverUrl = "http://internal.bi.verisk.com/spotfire/wp/";
+var c_analysisPath = "/ACE India/Public/Standardisation_Demo_BI_Portal";
+var c_analysisPath2 = "/ACE India/Public/Standardisation_Demo_BI_Portal_2";
 
-	var customization = new spotfire.webPlayer.Customization();
-	//Hide UI elements
-	customization.showCustomizableHeader = false;
-	customization.showStatusBar = false;
-	customization.showToolBar = false;
-	customization.showPageNavigation = false;
-	customization.showFilterPanel = false;
-	customization.showDodPanel = false;
-	var c_reloadAnalysisInstance = false;
-	
-	var c_analysisPath = analysisPath;
-	var c_divID = divID;
-	var c_vizDetails = vizDetails;
-	var res = vizDetails.split("=");
-	var tab = res[0];
-	var viz = res[1];
-	var c_parameters = 'flattenDXP="Y";VizTabName="'+tab+'";vizName="'+viz+'";';
-	console.log(c_parameters)
-	
-	var app;
-	app = new spotfire.webPlayer.Application(c_serverUrl, customization, c_analysisPath, c_parameters, c_reloadAnalysisInstance);
-	app.onError(errorCallback);
-	app.onOpened(onOpened);
-	console.log("Opening document at : " + (new Date).toLocaleTimeString());
 
-	DemoDashboard = app.openDocument(c_divID, c_vizDetails, customization);
-	DemoDashboard.onDocumentReady(onDocumentReady);
-}
+var customization = new spotfire.webPlayer.Customization();
+//Hide UI elements
+customization.showCustomizableHeader = false;
+customization.showStatusBar = false;
+customization.showToolBar = false;
+customization.showPageNavigation = false;
+customization.showFilterPanel = false;
+customization.showDodPanel = false;
+
+var c_parameters = 'flattenDXP="Y";VizTabName="Page1";vizName="Bar Chart (Dashboard 1)";';
+var c_parameters2 = 'flattenDXP="Y";VizTabName="Page1";vizName="Line Chart (Dashboard 2)";';
+var c_reloadAnalysisInstance = false;
+
+
+var app, app2;
+
+app = new spotfire.webPlayer.Application(c_serverUrl, customization, c_analysisPath, c_parameters, c_reloadAnalysisInstance);
+app2 = new spotfire.webPlayer.Application(c_serverUrl, customization, c_analysisPath2, c_parameters2, c_reloadAnalysisInstance);
+
+// Register error callback.
+app.onError(errorCallback);
+app2.onError(errorCallback);
+app.onOpened(onOpened);
+app2.onOpened(onOpened);
+
+console.log("Opening document at : " + (new Date).toLocaleTimeString());
+
+DemoDashboard = app.openDocument("container", "Page1=Bar Chart (Dashboard 1)", customization);
+DemoDashboard.onDocumentReady(onDocumentReady);
+
+DemoDashboard2 = app2.openDocument("container2", "Page1=Line Chart (Dashboard 2)", customization);
+DemoDashboard2.onDocumentReady(onDocumentReady);
 
 //
 // Web Player Callbacks
 //
 function onOpened(analysisDocument){
-    console.log("Document load completed at : " + (new Date).toLocaleTimeString());
+    //console.log("Document load completed at : " + (new Date).toLocaleTimeString());
 }
 function errorCallback(errorCode, description){
     // Log error message if something goes wrong in the Web Player.
@@ -48,5 +49,5 @@ function errorCallback(errorCode, description){
 }
 
 function onDocumentReady(){
-    console.log('ready');
+    //console.log('ready');
 }
